724962a93fe47a9bd7b4eb47c3285a4d6f78a19d
Caitlyn Barrett <90736239+caitynb@users.noreply.github.com>
2022-11-16 18:08:53 -0600

    Update GFK.R

--- GFK.R
+++ GFK.R
@@ -29,42 +29,40 @@ gfkwhole<-gfksub[(!is.na(gfksub$cons1)&!is.na(gfksub$cons2)
 #whole population
 wholecor<-cor(gfkwhole[,1:4])
 lowerwhole<-wholecor[lower.tri(wholecor)]
-wh<-round(mean(lowerwhole), digits=2)
+wh<-format(round(mean(lowerwhole), digits=2),nsmall=2)
 
 #white sample
 gfkwhite<-subset(gfkwhole, race==1)
 whitecor<-cor(gfkwhite[,1:4])
 lowerwhite<-whitecor[lower.tri(whitecor)]
-wi<-round(mean(lowerwhite), digits=2)
+wi<-format(round(mean(lowerwhite), digits=2),nsmall=2)
 
 #black sample
 gfkblack<-subset(gfkwhole, race==2)
 blackcor<-cor(gfkblack[,1:4])
 lowerblack<-blackcor[lower.tri(blackcor)]
-b<-round(mean(lowerblack), digits=2)
+b<-format(round(mean(lowerblack), digits=2),nsmall=2)
 
 #hispanic sample
 gfkhisp<-subset(gfkwhole,race==3)
 hispcor<-cor(gfkhisp[,1:4])
 lowerhisp<-hispcor[lower.tri(hispcor)]
-h<-round(mean(lowerhisp), digits=2)
-
+h<-format(round(mean(lowerhisp), digits=2),nsmall=2)
 
 
 ###################correlation figure
 #create dataframe for figure
 average<-c(wh, wi, b, h)
-race<-c("Whole","White","Black","Hispanic")
+race<-c("Whole","White","Black","Latinx")
 dat<-cbind.data.frame(average,race)
 
 #relevel to ensure ordering on ggplot matches
-dat$race<-factor(dat$race, levels=c("Whole","White","Black","Hispanic"))
-
+dat$race<-factor(dat$race, levels=c("Whole","White","Black","Latinx"))
+dat$avg<-as.numeric(dat$average)
 #ggplot coding
-anesplot<-ggplot(dat, aes(x=race, y=average,fill=race))+geom_bar(stat="identity")+theme_classic()+
-  geom_text(aes(label=average), vjust=1.5, color="black",position = position_dodge(0.9), size=4)
-anesplot<-anesplot + labs(title="Average Correlation for Conservation Values \nby Race (Figure 1)",subtitle="",y="Average Correlation",x="Race")+
-  theme(legend.position="",legend.title=element_blank())+
-  scale_fill_manual(values = c("Whole"="gray90", "White"="gray70", "Black"="gray50","Hispanic"="gray35"))
+anesplot<-ggplot(dat, aes(x=race, y=avg,fill=race))+geom_bar(stat="identity",position="dodge")+theme_classic()+
+  geom_text(aes(label=average), vjust=-0.2,color="black", size=3.5)
+anesplot<-anesplot + labs(y="Mean Correlation",x="",title="")+theme(legend.position="",legend.title=element_blank())+
+  scale_fill_manual(values = c("Whole"="gray90", "White"="gray70", "Black"="gray50","Latinx"="gray35"))+scale_y_continuous(labels = scales::number_format(accuracy = 0.01),limits=c(0,0.45))
 anesplot
-ggsave("Goren_Figure3.pdf")
+ggsave("ConsCorr_Figure.pdf")
